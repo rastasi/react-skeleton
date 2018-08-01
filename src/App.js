52b783c95558e3ae629edb1ce4af34e1519795bb
Zsolt Tasnadi <rastasi@gmail.com>
2018-08-01 23:05:57 +0200

    set example action to App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import connect from 'redux-connect-decorator'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Layout from './Layout';
+import {actions} from './store/actions';
 
 @connect(state => ({
   exampleValue: state.testGroup.exampleValue
-}))
+}), {
+  setExampleValue: actions.setExampleValue
+})
 class App extends Component {
   render() {
     return (
