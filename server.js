50cc84a38a74d551f7a7bfd779552edc1c31b832
Zsolt Tasnadi <rastasi@gmail.com>
2018-08-01 12:23:15 +0200

    remove unused files

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
+const exphbs = require('express-handlebars');
+app.set('views', `${__dirname}/views`);
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+app.set('view engine', 'handlebars');
+
+
+app.get('/**', (req, res) => {
+  res.render('index');
+});
+
 app.listen(8080);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 
+import App from './App';
 import store from './store'
 
 ReactDOM.render(
@@ -12,4 +11,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
